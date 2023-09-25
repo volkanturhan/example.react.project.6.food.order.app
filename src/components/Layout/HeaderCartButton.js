c84699d9a9f3faea2d364104d6c4d4a7fba6579c
Volkan Turhan <volkanturhan@msn.com>
2023-09-25 16:52:59 +0300

    3. commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import CardIcon from "../Cart/CardIcon";
+import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
@@ -10,7 +10,7 @@ const HeaderCartButton = (props) => {
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
-        <CardIcon />
+        <CartIcon />
       </span>
       <span >Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
